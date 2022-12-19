0250d73f1f2f3b9d45c6d7deff14189e44b3bc7f
sharmaDeepanshi <119604314+sharmaDeepanshi@users.noreply.github.com>
2022-12-19 23:47:51 +0530

    Add files via upload

---
+++ getch.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ char a,b,c;
+  clrscr();
+  printf("enter any charcacter:\n");
+  a=getchar();
+  printf("%c\n",a);
+  printf("enter any character:\n");
+  b=getche();
+  printf("%c\n",b);
+  printf("enter any character:\n");
+  c=getch();
+  printf("%c",c);
+
+}
